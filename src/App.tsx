752ca89b7057eac493f328f4ba0e2d88c5b93014
wooc <wooc@cz-robots.com>
2025-12-31 17:49:37 +0800

    init: Init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from 'antd';
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button type="primary">Button</Button>
+    </>
+  )
+}
+
+export default App
